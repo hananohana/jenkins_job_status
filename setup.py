79f5421d9b2b4e184884d67b78dd1eba6f4e25b0
Hanan Ohana <hanano@pliops.com>
2023-02-12 17:19:59 +0200

    update setup.py to install correct packages version

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ def install(package):
 
 
 what_python = float(sys.version[:3])
-
-install('urllib3==1.26.12')
-install('requests==2.28.1')
+print(what_python)
 
 if what_python >= 3.7:
+    print("Installing packages for python 3.7 and up")
     install('customtkinter==5.0.3')
+    install('requests==2.28.1')
+    install('urllib3==1.26.12')
+else:
+    print("Installing packages for up to python 3.6.9")
+    install('requests==2.27.1')
+    install('urllib3==1.26.12')
